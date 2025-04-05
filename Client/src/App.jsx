bf89fbb33218c8248e3cb265f61b7334de1551ce
Hifumi <perororo@proton.me>
2025-04-05 09:26:37 -0400

    refactor: move css to styles folder

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,10 @@
-import DefaultPage from "./components/defaultPage/DefaultPage.jsx"
+import DefaultPage from "./components/DefaultPage.jsx"
 import DailyCommissions from "./components/DailyCommissions.jsx"
 
 export default function App() {
-  return(
-    <>
-      <DefaultPage></DefaultPage>
-    </>
-  );
+    return (
+        <>
+            <DefaultPage></DefaultPage>
+        </>
+    )
 }
--- Client/src/components/DailyCommissions.jsx
+++ Client/src/components/DailyCommissions.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import "./DailyCommissions.css"
+import { useState } from "react"
 
 function DailyCommissions() {
     function getRandomInt(min, max) {
@@ -8,10 +7,12 @@ function DailyCommissions() {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
-    let isCommissionFinish = false;
-    const commissions = [{commissionName: "Study 1h with the study timer", commissionExp: 10}];
-    const [commissionToComplete, setCommissionToComplete] = useState(commissions[getRandomInt(0, commissions.length-1)].commissionName);
-    return(
+    let isCommissionFinish = false
+    const commissions = [{ commissionName: "Study 1h with the study timer", commissionExp: 10 }]
+    const [commissionToComplete, setCommissionToComplete] = useState(
+        commissions[getRandomInt(0, commissions.length - 1)].commissionName
+    )
+    return (
         <div>
             <header>Today Commission:</header>
             <h4>--- {commissionToComplete} (Not done)</h4>
