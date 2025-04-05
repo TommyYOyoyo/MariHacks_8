a0bda283d4e350253a953a703348edbb412e02b9
zebra-A <ziboz2009@gmail.com>
2025-04-04 22:27:37 -0400

    Add daily commission page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import DailyCommissions from "./components/DailyCommissions.jsx";
 import DefaultPage from "./DefaultPage.jsx"
 
 export default function App() {
-  return <DefaultPage></DefaultPage>
+  return(
+    <>
+      <DefaultPage></DefaultPage>
+    </>
+  );
 }
 
 /*import { useState } from 'react';
