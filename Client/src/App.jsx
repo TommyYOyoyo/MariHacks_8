d889c8237e9d932c3298223a6779921f0cff5294
Hifumi <perororo@proton.me>
2025-04-05 13:14:10 -0400

    chore: all

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,20 @@
-import DefaultPage from "./components/defaultPage/DefaultPage.jsx"
-import Register from "./components/defaultPage/Register.jsx";
-import Login from "./components/defaultPage/Login.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Login from "./pages/Login.jsx"
+import Register from "./pages/Register.jsx"
+import DailyCommissions from "./pages/DailyCommissions.jsx"
+// import Calendar from "./components/Calendar.jsx"
+import StudyTimer from "./pages/StudyTimer.jsx"
 
-export default function App() {
-  return(   
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/register" element={<Register/>} />
-                <Route path="/login" element={<Login/>} />
-            </Routes>
-        </BrowserRouter>
-    </>
-  );
-}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        children: [
+            { path: "login", element: <Login /> },
+            { path: "register", element: <Register /> }
+        ]
+    }
+])
+
+createRoot(document.getElementById("root")).render(<RouterProvider router={router} />)
