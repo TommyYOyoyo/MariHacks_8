b88f15fb5f7412b27ef54c9823053a4050cc766a
Hifumi <perororo@proton.me>
2025-04-04 23:49:22 -0400

    refactor: reformat

--- src/components/DailyCommissions.jsx
+++ src/components/DailyCommissions.jsx
@@ -1,26 +1,21 @@
 import "./DailyCommissions.css"
 
-function DailyCommissions()
-{
+function DailyCommissions() {
     function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
-    let isCommissionFinish = false;
-    const commissions = [{commissionName: "Study 1h with the study timer", commissionExp: 10}];
-    const commissionToComplete = commissions[getRandomInt(0, commissions.length-1)].commissionName;
-    return(
+    let isCommissionFinish = false
+    const commissions = [{ commissionName: "Study 1h with the study timer", commissionExp: 10 }]
+    const commissionToComplete = commissions[getRandomInt(0, commissions.length - 1)].commissionName
+    return (
         <div>
-            <header>
-                Today Commission: 
-            </header>
-            <h4>
-                --- {commissionToComplete} (Not done)
-            </h4>
+            <header>Today Commission:</header>
+            <h4>--- {commissionToComplete} (Not done)</h4>
         </div>
-    );
+    )
 }
 
 export default DailyCommissions
--- src/components/StudyTimer.jsx
+++ src/components/StudyTimer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 
 const StudyTimer = () => {
-  const studyDuration = 25 * 60 // 25 minutes
-  const breakDuration = 5 * 60 // 5 minutes
+    const studyDuration = 25 * 60 // 25 minutes
+    const breakDuration = 5 * 60 // 5 minutes
 }
