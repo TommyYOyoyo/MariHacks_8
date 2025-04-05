bd1ad35a5dee089cbeac41a57f332975e0ab1ca9
TommyYOyoyo <79941840+TommyYOyoyo@users.noreply.github.com>
2025-04-05 13:58:26 -0400

    added home page

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,45 +1,46 @@
-import { useState } from "react"
-import axios from "axios"
-import { Link } from "react-router-dom"
+import "../../styles/defaultPage.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function Login() {
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
+function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const navigate = useNavigate();
 
-    const handleSubmit = e => {
-        e.preventDefault()
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-        axios
-            .post("http://localhost:8080/login", { email, password })
-            .then(res => {
-                if (res.data == "Incorrect password") {
-                    document.querySelector("#loginWarning").style.display = "block"
-                    document.querySelector("#loginWarning").style.color = "red"
-                    document.querySelector("#loginWarning").innerHTML = "*Incorrect password"
-                } else if (res.data == "User not found") {
-                    document.querySelector("#loginWarning").style.display = "block"
-                    document.querySelector("#loginWarning").style.color = "red"
-                    document.querySelector("#loginWarning").innerHTML = "*User not found"
-                } else {
-                    document.querySelector("#loginWarning").style.display = "none"
-                    console.log(res.data)
-                    localStorage.setItem("sessionData", JSON.stringify(res.data))
-                }
-            })
-            .catch(err => console.log(err))
+        axios.post("http://localhost:8080/login", { email, password })
+        .then(res => {
+            if (res.data == "Incorrect password") {
+                document.querySelector("#loginWarning").style.display = "block";
+                document.querySelector("#loginWarning").style.color = "red";
+                document.querySelector("#loginWarning").innerHTML = "*Incorrect password";
+            } else if (res.data == "User not found") {
+                document.querySelector("#loginWarning").style.display = "block";
+                document.querySelector("#loginWarning").style.color = "red";
+                document.querySelector("#loginWarning").innerHTML = "*User not found";
+            } else {
+                localStorage.setItem("sessionData", JSON.stringify(res.data));
+                navigate("/homePage");
+            }
+        })
+        .catch(err => console.log(err));
     }
 
     return (
-        <div className="auth-page">
+        <div className="defaultPage">
+            <link rel="stylesheet" href="./defaultPage.css"></link>
             <h1 id="title">COLLABILITY</h1>
 
-            <form className="auth-form" onSubmit={handleSubmit}>
+            <form id="form" onSubmit={handleSubmit}>
                 <label>Email</label>
                 <input
                     type="text"
                     id="email"
                     placeholder="abc@gmail.com"
-                    onChange={e => setEmail(e.target.value)}
+                    onChange={(e) => setEmail(e.target.value)}
                 ></input>
 
                 <div className="passwordBox">
@@ -49,15 +50,17 @@ export default function Login() {
                             type="password"
                             id="password"
                             placeholder="Your password"
-                            onChange={e => setPassword(e.target.value)}
+                            onChange={(e) => setPassword(e.target.value)}
                         ></input>
                     </div>
                     <label id="loginWarning" className="warning"></label>
                 </div>
 
                 <div id="options">
-                    <div className="redirects">
-                        <Link to="/register">Register</Link>
+                    <div id="redirects">
+                        <div id="registerRedirect">
+                            <a href="./register">Register</a>
+                        </div>
                     </div>
 
                     <div id="rememberMeBox">
@@ -69,5 +72,7 @@ export default function Login() {
                 <input type="submit" value="Login" id="submitButton"></input>
             </form>
         </div>
-    )
+    );
 }
+
+export default Login;
