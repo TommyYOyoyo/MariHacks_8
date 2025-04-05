85a8dea567d88f8e6d1ad239f3b4ca62cfd0a6d3
zebra-A <ziboz2009@gmail.com>
2025-04-05 13:24:33 -0400

    Fix issue

--- Client/src/components/container/Container.jsx
+++ Client/src/components/container/Container.jsx
@@ -1,21 +1,61 @@
-import React from "react";
+import React from 'react';
+import Board from '../board/Board';
 
-class Container extends React.Component {
+import './style.css';
+
+class Container extends React.Component
+{
     constructor(props) {
         super(props);
+
+        this.state = {
+            color: "#000000",
+            size: "5"
+        }
     }
 
-    render () {
+    changeColor(params) {
+        this.setState({
+            color: params.target.value
+        })
+    }
+
+    changeSize(params) {
+        this.setState({
+            size: params.target.value
+        })
+    }
+
+    render() {
+
         return (
             <div className="container">
-                <div className="color-picker-container">
-                    <input type="color"/>
+                <div class="tools-section">
+                    <div className="color-picker-container">
+                        Select Brush Color : &nbsp; 
+                        <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
+                    </div>
+
+                    <div className="brushsize-container">
+                        Select Brush Size : &nbsp; 
+                        <select value={this.state.size} onChange={this.changeSize.bind(this)}>
+                            <option> 5 </option>
+                            <option> 10 </option>
+                            <option> 15 </option>
+                            <option> 20 </option>
+                            <option> 25 </option>
+                            <option> 30 </option>
+                        </select>
+                    </div>
+
                 </div>
 
                 <div class="board-container">
-                    <Board></Board>
+                    <Board color={this.state.color} size={this.state.size}></Board>
                 </div>
             </div>
         )
     }
 }
+
+export default Container
