1ec7541d350f71ffe50d5a79997f590ca1d0b132
Lela Star <lelastar8888@gmail.com>
2025-04-05 10:27:30 -0400

    calendar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,8 @@
 import DefaultPage from "./components/DefaultPage.jsx"
+import React from 'react';
 import DailyCommissions from "./components/DailyCommissions.jsx"
+import Calendar from "./components/Calendar.jsx"
+import StudyTimer from "./components/StudyTimer.jsx"
 
 export default function App() {
     return (
